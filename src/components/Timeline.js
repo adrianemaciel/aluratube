18f1ec0aad08038e56c64b119af14723272c8012
adrianemaciel <adrianemaciel.developer@gmail.com>
2022-11-08 14:45:34 -0300

    timeline refactor

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledTimeline = styled.div`
   flex: 1;
   width: 100%;
-  padding: 16px;
+  /* padding: 16px; */
   overflow: hidden; 
   h2 {
     font-size: 16px;
@@ -30,7 +30,7 @@ export const StyledTimeline = styled.div`
       grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
       grid-auto-flow: column;
       grid-auto-columns: minmax(200px, 1fr);
-      overflow-x: scroll;
+      /* overflow-x: scroll; */
       scroll-snap-type: x mandatory;
       a {
         scroll-snap-align: start;
